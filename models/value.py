9e4722434c5fa88f957860da2f7c5a6e37b4f9fb
Mouhoub Massinissa <massipaylancer@gmail.com>
2025-02-18 21:39:34 +0100

    add gradients for second example

--- models/value.py
+++ models/value.py
@@ -4,6 +4,7 @@ class Value:
     def __init__(self, data, _children=(), _op='', label=''):
         self.data = data
         self.grad = 0.0
+        self._backward = lambda: None
         self._prev = set(_children)
         self._op = _op
         self.label = label
