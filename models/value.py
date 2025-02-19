378cc36acce93af9ec30b9352cc07ac097629307
Mouhoub Massinissa <massipaylancer@gmail.com>
2025-02-19 22:10:19 +0100

    add substraction, exp, power and division in the Value class

--- models/value.py
+++ models/value.py
@@ -13,6 +13,7 @@ class Value:
         return f"Value(data={self.data}, grad={self.grad})"
 
     def __add__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data + other.data, (self, other), '+')
 
         def _backward():
@@ -24,6 +25,7 @@ class Value:
         return out
 
     def __mul__(self, other):
+        other = other if isinstance(other, Value) else Value(other)
         out = Value(self.data * other.data, (self, other), '*')
 
         def _backward():
@@ -32,6 +34,26 @@ class Value:
             
         out._backward = _backward
         return out
+    
+    def __pow__(self, other):
+        assert isinstance(other, (int, float)), "Only supporting int/float powers."
+        out = Value(self.data**other, (self,), f'**{other}')
+        
+        def _backward():
+            self.grad += other * self.data ** (other-1) * out.grad
+        out._backward = _backward
+           
+    def __rmul__(self, other):
+        return self * other
+    
+    def __truediv__(self, other):
+        return self * other**-1
+    
+    def __neg__(self):
+        return self * -1
+
+    def __sub__(self, other):
+        return self + (-other)
 
     def tanh(self):
         x = self.data
@@ -43,6 +65,16 @@ class Value:
         
         out._backward = _backward
         
+        return out
+    
+    def exp(self):
+        x = self.data
+        out = Value(math.exp(x), (self,), 'exp')
+        
+        def _backward():
+            self.grad += out.data * out.grad
+            
+        out._backward()
         return out
     
     def backward(self):
